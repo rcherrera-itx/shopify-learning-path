7504433409d0ffb8e22be494b0b15e9329f40e13
Rafael Contreras Herrera <rcherrera@itx.com>
2026-08-28 16:04:09 -0600

    Created Server Component to get Buyer IP.

---
+++ src/shopify/buyer-ip.ts
@@ -0,0 +1,14 @@
+import "server-only";
+
+import { headers } from "next/headers";
+
+export async function getBuyerIpHeaders(): Promise<
+    Record<string, string>
+> {
+    const requestHeader = await headers();
+    const forwardedFor = requestHeader.get("x-formward-for");
+
+    const buyerIp = forwardedFor?.split(",")[0]?.trim() || requestHeader.get("x-real-ip");
+
+    return buyerIp ? { "Shopify-Storefront-Buyer-IP": buyerIp } : {};
+}
