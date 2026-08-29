7c45256b43adfaafcb6fe9592975b3feafd5d402
Rafael Contreras Herrera <rcherrera@itx.com>
2026-08-28 23:06:26 -0600

    Added Cart Checkout Url Query.

--- src/shopify/queries/cart.ts
+++ src/shopify/queries/cart.ts
@@ -2,7 +2,6 @@ import "server-only";
 import { getBuyerIpHeaders } from "../buyer-ip";
 import { storefrontClient } from "../client";
 
-
 const CART_QUERY = `#graphql
     query Cart($id: ID!) {
         cart(id: $id) {
@@ -102,4 +101,44 @@ export async function getCart(
     }
 
     return data?.cart ?? null;
+}
+
+
+const CART_CHECKOUT_URL_QUERY = `#graphql
+    query CartCheckoutUrl($id: ID!) {
+        cart(id: $id) {
+            checkoutUrl
+        }
+    }
+`;
+
+type CartCheckoutUrlData = {
+    cart: {
+        checkoutUrl: string;
+    }
+}
+
+export async function getCheckoutUrl(
+    cartId: string,
+): Promise<string | null> {
+    const buyerIpHeaders = await getBuyerIpHeaders();
+
+    const { data, errors } = await storefrontClient.request<CartCheckoutUrlData>(
+        CART_CHECKOUT_URL_QUERY,
+        {
+            variables: {
+                id: cartId,
+            },
+            headers: buyerIpHeaders,
+        },
+    );
+
+    if (errors) {
+        const messages = errors.graphQLErrors?.map((error) => error.message).join(" | ");
+        throw new Error(
+            messages ?? errors.message ?? "Shopify cart query failed."
+        );
+    }
+
+    return data?.cart?.checkoutUrl ?? null;
 }
